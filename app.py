cbee1c952b299033d5e3aae007e4aeb49f379552
Paula Samanta <73899414+paula-33@users.noreply.github.com>
2021-12-17 21:07:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ classifier = pickle.load(pickle_in)
 def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
  
     # Pre-processing user input    
-    if Gender == "Male":
+    if Gender == "Masculino":
         Gender = 0
     else:
         Gender = 1
  
-    if Married == "Unmarried":
+    if Married == "Solteiro":
         Married = 0
     else:
         Married = 1
  
-    if Credit_History == "Unclear Debts":
+    if Credit_History == "Com dividas":
         Credit_History = 0
     else:
         Credit_History = 1  
@@ -44,7 +44,7 @@ def main():
     # front end elements of the web page 
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
+    <h1 style ="color:black;text-align:center;">Consulta de emprestimo</h1> 
     </div> 
     """
       
@@ -52,11 +52,11 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True) 
       
     # following lines create boxes in which user can enter data required to make prediction 
-    Gender = st.selectbox('Gender',("Male","Female"))
-    Married = st.selectbox('Marital Status',("Unmarried","Married")) 
-    ApplicantIncome = st.number_input("Applicants monthly income") 
-    LoanAmount = st.number_input("Total loan amount")
-    Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
+    Gender = st.selectbox('Gender',("Masculino","Feminino"))
+    Married = st.selectbox('Marital Status',("Solteiro","Casado")) 
+    ApplicantIncome = st.number_input("Renda Mensal") 
+    LoanAmount = st.number_input("Valor Emprestimo")
+    Credit_History = st.selectbox('Credit_History',("Com dividas","Sem dividas"))
     result =""
       
     # when 'Predict' is clicked, make the prediction and store it 
